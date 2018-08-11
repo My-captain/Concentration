37491a5870807c9cae1133ca41305200ddf98a27
Mr.Robot <mr.robot@MrRobotdeMac.local>
2018-08-11 23:21:55 +0800

    Initial commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mr.Robot on 2018/8/11.
+//  Copyright © 2018年 Mr.Robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
